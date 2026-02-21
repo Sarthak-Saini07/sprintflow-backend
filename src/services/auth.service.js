8b84185b4f68bd721d1fff82ebfd9ad600d8ebd0
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-21 18:32:05 +0530

    feat: implement register and login service logic

---
+++ src/services/auth.service.js
@@ -0,0 +1,48 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import generateToken from "../utils/generateToken.js";
+
+export const registerUser = async (data) => {
+  const { name, email, password } = data;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  const token = generateToken({
+    id: user._id,
+    role: user.role
+  });
+
+  return { user, token };
+};
+
+export const loginUser = async (data) => {
+  const { email, password } = data;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid credentials");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid credentials");
+  }
+
+  const token = generateToken({
+    id: user._id,
+    role: user.role
+  });
+
+  return { user, token };
+};
