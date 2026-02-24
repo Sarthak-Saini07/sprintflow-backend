8a1b2b33f0ae1c738e1b39ec26d5fc540fefa616
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-24 18:49:59 +0530

    controller added

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,51 @@
+// import bcrypt from "bcryptjs";
+// import User from "../models/user.model.js";
+// import generateToken from "../utils/generateToken.js";
+
+// export const registerUser = async (data) => {
+//   const { name, email, password } = data;
+
+//   const existingUser = await User.findOne({ email });
+//   if (existingUser) {
+//     throw new Error("User already exists");
+//   }
+
+//   const hashedPassword = await bcrypt.hash(password, 10);
+
+//   const user = await User.create({
+//     name,
+//     email,
+//     password: hashedPassword
+//   });
+
+//   const token = generateToken({
+//     id: user._id,
+//     role: user.role
+//   });
+
+//   return { user, token };
+// };
+
+// export const loginUser = async (data) => {
+//   const { email, password } = data;
+
+//   const user = await User.findOne({ email });
+//   if (!user) {
+//     throw new Error("Invalid credentials");
+//   }
+
+//   const isMatch = await bcrypt.compare(password, user.password);
+//   if (!isMatch) {
+//     throw new Error("Invalid credentials");
+//   }
+
+//   const token = generateToken({
+//     id: user._id,
+//     role: user.role
+//   });
+
+//   return { user, token };
+// };
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import generateToken from "../utils/generateToken.js";
@@ -23,7 +71,15 @@ export const registerUser = async (data) => {
     role: user.role
   });
 
-  return { user, token };
+  return {
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role
+    },
+    token
+  };
 };
 
 export const loginUser = async (data) => {
@@ -44,5 +100,13 @@ export const loginUser = async (data) => {
     role: user.role
   });
 
-  return { user, token };
+  return {
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role
+    },
+    token
+  };
 };
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,8 +1,21 @@
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (payload) => {
+//   return jwt.sign(payload, process.env.JWT_SECRET, {
+//     expiresIn: process.env.JWT_EXPIRES_IN
+//   });
+// };
+
+// export default generateToken;
 import jwt from "jsonwebtoken";
 
 const generateToken = (payload) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: process.env.JWT_EXPIRES_IN || "1d",
   });
 };
 
